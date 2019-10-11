0f86ec0d928a5923eef3e69675e8b5989f88f121
XD <514269236@qq.com>
2019-10-11 11:50:21 +0800

    实验1

--- 01/1_child_process.c
+++ 01/1_child_process.c
@@ -5,21 +5,21 @@
 int main()
 {
   pid_t  pid;
-	/* fork another process */
-	pid = fork();
-	if (pid < 0) { /* error occurred */
-		fprintf(stderr, "Fork Failed");
-		exit(-1);
-	}
-	else if (pid == 0) { /* child process */
-		printf("This is child process \n");
-		execlp("/bin/ls", "ls", NULL);
-	}
-	else { /* parent process */
-		/* parent will wait for the child to complete */
-		printf("This is parent process \n");
-		wait (NULL);
-		printf ("Child Complete \n");
-		exit(0);
-	}
+        /* 创建进程  */
+        pid = fork();
+        if (pid < 0) { /* 出现错误  */
+                fprintf(stderr, "Fork Failed");
+                exit(-1);
+        }
+        else if (pid == 0) { /* 子进程  */
+                printf("This is child process \n");
+        }
+        else { /* 父进程 */
+                /* 等待子进程完成 */
+                printf("This is parent process \n");
+                printf("Wait child process \n");
+                wait (NULL);
+                printf ("Child Complete \n");
+                exit(0);
+        }
 }
